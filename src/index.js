5e4bd358e8191c5816284bceb0fae2930ae02540
zgreathouse <greathouse.zachary@gmail.com>
2017-12-27 20:31:44 -0800

    create empty component files, build out index.js file in src folder, build skelton for search bar component

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 
+//components
+import SearchBar from './components/SearchBar';
+
+const API_KEY = 'AIzaSyCr7wq-csOieIqjuNGpdN9FdMoDlVvpsJo';
+
+const App = () => {
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
+}
+
+ReactDOM.render(<App/>, document.querySelector('.container'));

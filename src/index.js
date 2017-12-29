a6fbc0174186a6e3f0c5ccd4218ea5ac786b4d55
zgreathouse <greathouse.zachary@gmail.com>
2017-12-28 17:11:55 -0800

    refactor root component (App) to be a class component with local state, clean up SearchBar.js file

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+const API_KEY = 'AIzaSyCr7wq-csOieIqjuNGpdN9FdMoDlVvpsJo';
 
 //components
 import SearchBar from './components/SearchBar';
 
-const API_KEY = 'AIzaSyCr7wq-csOieIqjuNGpdN9FdMoDlVvpsJo';
+class App extends Component {
+  constructor(props) {
+    super(props);
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'chillhop'}, videos => {
+      this.setState({ videos });
+      console.log(this.state);
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(<App/>, document.querySelector('.container'));
